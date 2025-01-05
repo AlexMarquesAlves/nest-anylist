02b056949a36fcbbbea7c946eb72fa521691404e
¡Duque <atanaelleonardo@gmail.com>
2025-01-05 17:43:51 -0300

    feat: install typeOrm dependencies

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,8 @@
 export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'development',
-  dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   port: +process.env.PORT || 3002,
+  dbName: process.env.DB_NAME,
+  dbUserName: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
 })
