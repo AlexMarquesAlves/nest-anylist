b2efa8591dff025698ef4fac033b3a79730357cb
¡Duque <atanaelleonardo@gmail.com>
2025-01-06 18:18:40 -0300

    feat: `Add @IsOptional and @IsPositive decorators to limit and offset properties in PaginationDto`

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,4 +1,12 @@
+import { IsOptional, IsPositive } from 'class-validator'
+
 export class PaginationDto {
+  @IsOptional()
+  @IsPositive()
+  // transformer
   limit?: number
+
+  @IsOptional()
+  @IsPositive()
   offset?: number
 }
