bebad031fc4885067116c1b0f435ca42a0d18fb4
¡Duque <atanaelleonardo@gmail.com>
2025-01-06 10:16:08 -0300

    feat: `Refactor ProductsService to use TypeORM and add logging`

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { log } from 'console'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import type { Repository } from 'typeorm'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
+import { Product } from './entities/product.entity'
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    log('This action adds a new product')
+
+    try {
+      const product = this.productRepository.create(createProductDto)
+      await this.productRepository.save(product)
+
+      return product
+    } catch (error) {
+      log(error)
+      throw new InternalServerErrorException(error)
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return `This action returns all products`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    return `This action returns a #${id} product`
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    return `This action removes a #${id} product`
   }
 }
