0fca29a7c5941ac8c6e2bbb7f3ca3750683956c5
¡Duque <atanaelleonardo@gmail.com>
2025-01-07 09:49:40 -0300

    feat: `Added ProductsModule import and dependency injection to SeedModule and SeedService`

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, Logger } from '@nestjs/common'
+import { ProductsService } from '../products/products.service'
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
   async runSeed() {
     const seedMessage = 'SEED HAS BEEN EXECUTED'
-    Logger.log(`THE ${seedMessage} SUCCESSFULLY`)
 
+    try {
+      await this.insertNewProducts()
+
+      Logger.log(`THE ${seedMessage} SUCCESSFULLY`)
+    } catch (error) {
+      Logger.error(error)
+    }
     return { seedMessage }
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts()
   }
 }
