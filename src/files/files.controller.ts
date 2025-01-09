56b03064c2eeecc78498cca3c26014a2cf5fbaf5
¡Duque <atanaelleonardo@gmail.com>
2025-01-09 01:14:12 -0300

    feat: `upload a fle on backend`

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,42 +1,15 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common'
-import { CreateFileDto } from './dto/create-file.dto'
-import { UpdateFileDto } from './dto/update-file.dto'
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
 import { FilesService } from './files.service'
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto)
-  }
-
-  @Get()
-  findAll() {
-    return this.filesService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filesService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filesService.remove(+id)
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    // return this.filesService.uploadProductImage()
+    return file
   }
 }
