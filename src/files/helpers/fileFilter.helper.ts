0b9f3d77325b26f15ca172927ab550bd856c9c36
¡Duque <atanaelleonardo@gmail.com>
2025-01-09 01:49:36 -0300

    feat: `Added file validation and logging to fileFilter.helper.ts`

--- src/files/helpers/fileFilter.helper.ts
+++ src/files/helpers/fileFilter.helper.ts
@@ -1,9 +1,18 @@
+import { Logger } from '@nestjs/common'
+
 export const fileFilter = (
   req: Express.Request,
   file: Express.Multer.File,
   callBack: Function
 ) => {
-  console.log({ file })
+  // console.log({ file })
+  if (!file) return callBack(new Logger.error(`Field file is empty`), false)
 
-  callBack(null, true)
+  const fileExtension = file.mimetype.split('/')[1]
+  const validExtensions = ['jpg', 'jpeg', 'png', 'gif']
+  if (!validExtensions.includes(fileExtension)) {
+    return callBack(null, true)
+  }
+
+  callBack(null, false)
 }
