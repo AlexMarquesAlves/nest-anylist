474c7bf0f13bcc5452b3cbeba6d2920fc8241b22
¡Duque <atanaelleonardo@gmail.com>
2025-01-12 18:13:19 -0300

    feat: Added validation to CreateUserDto class

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -6,4 +6,21 @@ import {
   MinLength,
 } from 'class-validator'
 
-export class CreateUserDto {}
+export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  email: string
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string
+
+  @IsString()
+  @MinLength(3)
+  fullName: string
+}
