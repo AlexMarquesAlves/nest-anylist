c93d79ca529d1b0428a16777902f639ddd0d3614
¡Duque <atanaelleonardo@gmail.com>
2025-01-12 17:48:58 -0300

    feat: rename `Auth` entity into `User` entity and added `TypeOrmModule` to AuthModule`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { User } from './entities/user.entity'
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
 })
 export class AuthModule {}
