da597a8e2fb1d6b59f4e04165404fc505e112a11
¡Duque <atanaelleonardo@gmail.com>
2025-01-12 17:56:54 -0300

    feat(auth): Removed unused import and updated code formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module'
 import { CommonModule } from './common/common.module'
 import { FilesModule } from './files/files.module'
 import { ProductsModule } from './products/products.module'
 import { SeedModule } from './seed/seed.module'
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
