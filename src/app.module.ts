4e9bbaa420031da4e92fa5ac93824e91038d4726
¡Duque <atanaelleonardo@gmail.com>
2025-01-16 00:06:14 -0300

    feat: updated app.module.ts with Apollo Server plugin and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'node:path'
-import { HelloWorldModule } from './hello-world/hello-world.module';
+import { HelloWorldModule } from './hello-world/hello-world.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
-      // playground: false
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloWorldModule,
   ],
