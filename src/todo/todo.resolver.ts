20a3bb9b08632c4761b0469555ba291f0c48a741
¡Duque <atanaelleonardo@gmail.com>
2025-01-17 19:35:32 -0300

    feat: added TodoResolver with multiple query methods

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,29 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql'
 
 @Resolver()
-export class TodoResolver {}
+export class TodoResolver {
+  @Query(() => [String], { name: 'TODOs' })
+  findAll() {
+    return this.todoService.findAll()
+  }
+
+  @Query(() => [String])
+  findOne() {
+    return this.todoService.findOne()
+  }
+
+  @Query(() => [String])
+  createTodo() {
+    return this.todoService.createTodo()
+  }
+
+  @Query(() => [String])
+  updateTodo() {
+    return this.todoService.updateTodo()
+  }
+
+  @Query(() => [String])
+  removeTodo() {
+    return this.todoService.removeTodo()
+  }
+}
