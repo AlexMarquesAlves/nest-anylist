55b36b69d24fb8f92a474d4ce1b6bed07a8bbcee
¡Duque <atanaelleonardo@gmail.com>
2025-01-17 01:07:24 -0300

    feat: Update HelloWorldResolver to include randomNumber query and update schema.gql accordingly

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Float, Query, Resolver } from '@nestjs/graphql'
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: `hello`,
+    description: `Hola mundo es lo que retorna`,
+  })
   helloWorld(): string {
     return 'Hello, World!'
+  }
+
+  @Query(() => Float, { name: `randomNumber` })
+  getRandomNumber(): number {
+    return Math.random() * 100
   }
 }
