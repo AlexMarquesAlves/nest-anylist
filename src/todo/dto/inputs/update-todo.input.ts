e6ee1ca7e034d00b53a88eb33ba3e440dce22d86
¡Duque <atanaelleonardo@gmail.com>
2025-01-19 13:36:14 -0300

    feat: update UpdateTodoInput class with additional validation decorators

--- src/todo/dto/inputs/update-todo.input.ts
+++ src/todo/dto/inputs/update-todo.input.ts
@@ -1,5 +1,13 @@
 import { Field, InputType, Int } from '@nestjs/graphql'
-import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator'
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator'
 
 @InputType()
 export class UpdateTodoInput {
@@ -15,8 +23,11 @@ export class UpdateTodoInput {
   @IsString()
   @IsNotEmpty()
   @MaxLength(20)
+  @IsOptional()
   description?: string
 
   @Field(() => Boolean, { nullable: true })
+  @IsBoolean()
+  @IsOptional()
   done?: boolean
 }
