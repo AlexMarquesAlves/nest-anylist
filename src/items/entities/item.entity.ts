52df23975391b29c0bbd4fbf9ac0740146dd4ec0
¡Duque <atanaelleonardo@gmail.com>
2025-01-21 03:08:27 -0300

    feat: add GraphQL decorators to Item entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,19 +1,23 @@
-import { ObjectType } from '@nestjs/graphql'
+import { Field, Float, ID, ObjectType } from '@nestjs/graphql'
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn('uuid') // * TypeORM decorators
+  @Field(() => ID) // * GraphQL decorators
   id: string
 
-  @Column()
+  @Column() // * TypeORM decorators
+  @Field(() => String) // * GraphQL decorators
   name: string
 
-  @Column()
+  @Column() // * TypeORM decorators
+  @Field(() => Float) // * GraphQL decorators
   quantity: number
 
-  @Column()
+  @Column() // * TypeORM decorators
+  @Field(() => String) // * GraphQL decorators
   quantityUnits: string //  g, ml, kg, tsp
 
   // ? stores
