908b9ce462d57939df0144931aec2e084ce02cdc
¡Duque <atanaelleonardo@gmail.com>
2025-01-21 17:49:56 -0300

    feat: add Logger to ItemsService for logging operations

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import type { Repository } from 'typeorm'
 import { CreateItemInput, UpdateItemInput } from './dto/inputs'
@@ -12,31 +12,40 @@ export class ItemsService {
   ) {}
 
   async create(createItemInput: CreateItemInput): Promise<Item> {
+    const logger = new Logger('Create Service')
     const newItem = this.itemsRepository.create(createItemInput)
 
+    logger.log(` New item "${newItem.name}" was successfully created`)
     return await this.itemsRepository.save(newItem)
   }
 
   async findAll(): Promise<Item[]> {
+    const logger = new Logger('Find All Service')
     // TODO: filtrar, paginar, por usuario...
+
+    logger.log(`Searching for all items`)
     return await this.itemsRepository.find()
   }
 
   async findOne(id: string): Promise<Item> {
+    const logger = new Logger('Find One Service')
     const item = await this.itemsRepository.findOneBy({ id })
     if (!item) {
-      throw new NotFoundException(`Item with id ${id} not found`)
+      throw new NotFoundException(`Item with id: ${id} not found`)
     }
 
+    logger.log(`Item with ID: ${id} was successfully found`)
     return item
   }
 
   async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+    const logger = new Logger('Update Service')
     const item = await this.itemsRepository.preload(updateItemInput)
     if (!item) {
-      throw new NotFoundException(`Item with id ${id} not found`)
+      throw new NotFoundException(`Item with id: ${id} not found`)
     }
 
+    logger.log(`Item with ID: ${id} was successfully updated`)
     return await this.itemsRepository.save(item)
   }
 
