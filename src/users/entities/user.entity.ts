be79c0c2507d1156e9b360ea67ccefd609e7e25d
¡Duque <atanaelleonardo@gmail.com>
2025-01-21 22:18:50 -0300

    feat: add User  entity with TypeORM and GraphQL decorators.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,32 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
-@ObjectType()
+@Entity({ name: 'users' }) // * TypeORM Decorators
+@ObjectType() // * GraphQl Decorators
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid') // * TypeORM Decorators
+  @Field(() => ID) // * GraphQl Decorators
+  id: string
+
+  @Column() // * TypeORM Decorators
+  @Field(() => String) // * GraphQl Decorators
+  fullName: string
+
+  @Column({ unique: true }) // * TypeORM Decorators
+  @Field(() => String) // * GraphQl Decorators
+  email: string
+
+  @Column() // * TypeORM Decorators
+  // ? @Field(() => String) // * GraphQl Decorators
+  password: string
+
+  @Column({ type: 'text', array: true, default: ['user'] }) // * TypeORM Decorators
+  @Field(() => [String]) // * GraphQl Decorators
+  roles: string[]
+
+  @Column({ type: 'boolean', default: true }) // * TypeORM Decorators
+  @Field(() => Boolean) // * GraphQl Decorators
+  isActive: boolean
+
+  // TODO: relaciones y otras cosas...
 }
