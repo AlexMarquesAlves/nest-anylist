c92f8b6dda9ff445146c8b526e923ab2f8c85eea
¡Duque <atanaelleonardo@gmail.com>
2025-01-26 20:18:21 -0300

    feat(auth): revalidate token functionality implemented in auth resolver and service

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,16 +1,21 @@
-import { ExecutionContext, InternalServerErrorException, createParamDecorator } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
+import {
+  ExecutionContext,
+  InternalServerErrorException,
+  createParamDecorator,
+} from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
 
 export const CurrentUser = createParamDecorator(
-    ( roles =[], context: ExecutionContext ) => {
+  (_roles = [], context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context)
+    const user = ctx.getContext().req.user
 
-        const ctx = GqlExecutionContext.create( context )
-        const user = ctx.getContext().req.user;
+    if (!user) {
+      throw new InternalServerErrorException(
+        `No user inside the request - make sure that we used the AuthGuard`
+      )
+    }
 
-        if( !user ) {
-            throw new InternalServerErrorException(`No user inside the request - make sure that we used the AuthGuard`)
-        }
-
-        return user;
-
-})
+    return user
+  }
+)
