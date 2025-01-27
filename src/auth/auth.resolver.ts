737ae3829bb9429d8bf7ee767608415e07601517
¡Duque <atanaelleonardo@gmail.com>
2025-01-27 00:05:51 -0300

    feat: updated settings.json and auth resolver to include icon theme and custom file associations

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -29,7 +29,11 @@ export class AuthResolver {
 
   @Query(() => AuthResponse, { name: 'revalidate' })
   @UseGuards(JwtAuthGuard)
-  revalidateToken(@CurrentUser() user: User): AuthResponse {
+  revalidateToken(
+    @CurrentUser()
+    // * [ValidRoles.admin]
+    user: User
+  ): AuthResponse {
     return this.authService.revalidateToken(user)
   }
 }
--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,14 +1,17 @@
 import {
   ExecutionContext,
+  ForbiddenException,
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common'
 import { GqlExecutionContext } from '@nestjs/graphql'
+import type { User } from 'src/users/entities/user.entity'
+import type { ValidRoles } from '../enums/valid-roles.enum'
 
 export const CurrentUser = createParamDecorator(
-  (_roles = [], context: ExecutionContext) => {
+  (roles: ValidRoles[] = [], context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context)
-    const user = ctx.getContext().req.user
+    const user: User = ctx.getContext().req.user
 
     if (!user) {
       throw new InternalServerErrorException(
@@ -16,6 +19,17 @@ export const CurrentUser = createParamDecorator(
       )
     }
 
-    return user
+    if (roles.length === 0) return user
+
+    for (const role of user.roles) {
+      // TODO: Eliminar ValidRoles
+      if (roles.includes(role as ValidRoles)) {
+        return user
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} does not have the required role [${roles}]`
+    )
   }
 )
