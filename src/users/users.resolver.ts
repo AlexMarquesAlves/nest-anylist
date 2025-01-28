91bc843ffdcab645b6606ab716437b61ef881325
¡Duque <atanaelleonardo@gmail.com>
2025-01-27 22:45:54 -0300

    feat: update users.resolver.ts to use ParseUUIDPipe and add CurrentUser decorator

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { UseGuards } from '@nestjs/common'
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common'
 import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum'
 import { CurrentUser } from '../auth/decorators/current-user.decorator'
@@ -21,10 +21,11 @@ export class UsersResolver {
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
-    // TODO:
-    throw new Error('No implementado')
-    // return this.usersService.findOne(id);
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) _user: User
+  ): Promise<User> {
+    return this.usersService.findOneById(id)
   }
 
   // @Mutation(() => User)
