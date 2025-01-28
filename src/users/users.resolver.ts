a1b1857ff01b014a30a6e226c4394f9afa137ab9
¡Duque <atanaelleonardo@gmail.com>
2025-01-27 22:35:40 -0300

    feat: add authentication and authorization to UsersResolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,14 +1,22 @@
+import { UseGuards } from '@nestjs/common'
 import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum'
+import { CurrentUser } from '../auth/decorators/current-user.decorator'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 import { ValidRolesArgs } from './dto/args/roles.arg'
 import { User } from './entities/user.entity'
 import { UsersService } from './users.service'
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) _user: User
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles)
   }
 
