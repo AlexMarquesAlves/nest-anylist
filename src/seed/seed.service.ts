c93a4a27280a7e16fa0d3f20810c1d634c4c55c2
¡Duque <atanaelleonardo@gmail.com>
2025-01-30 00:49:40 -0300

    feat: add imports and database operations to SeedModule and SeedService

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Item } from 'src/items/entities/item.entity'
+import { User } from 'src/users/entities/user.entity'
+import type { Repository } from 'typeorm'
 
 const logger = new Logger('SeedService')
 
@@ -7,7 +11,15 @@ const logger = new Logger('SeedService')
 export class SeedService {
   private isProd: boolean
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {
     this.isProd = configService.get('STATE') === 'prod'
   }
 
@@ -16,19 +28,23 @@ export class SeedService {
       throw new UnauthorizedException('We cannot run SEED on production.')
     }
     // * Limpiar la base de datos BORRANDO TODO
+    await this.deleteDatabase()
 
-    await this.deleteAll()
     // * Crear los datos de ejemplo
     await this.createData()
 
+    logger.log('SeedService executed successfully')
     return true
   }
 
-  deleteAll() {
-    throw new Error('Method not implemented.')
+  async deleteDatabase() {
+    //! Delete all items
+    await this.itemsRepository.createQueryBuilder().delete().where({}).execute()
+    //! Delete all users
+    await this.usersRepository.createQueryBuilder().delete().where({}).execute()
   }
 
-  createData() {
+  async createData() {
     // * Crear usuarios
     // * Crear Items
   }
