46034866fad13bebad62db2c975c13cc37148f39
¡Duque <atanaelleonardo@gmail.com>
2025-02-02 16:42:26 -0300

    feat: add class-validator decorators and updated import for @nestjs/graphql

--- src/common/dto/args/search.args.ts
+++ src/common/dto/args/search.args.ts
@@ -1,8 +1,12 @@
-import { ArgsType } from '@nestjs/graphql'
+import { ArgsType, Field } from '@nestjs/graphql'
+import { IsOptional, IsString } from 'class-validator'
 
 @ArgsType()
 export class SearchArg {
   constructor() {}
 
+  @Field(() => String, { nullable: true }) // GraphQl decorators
+  @IsOptional() // Class-validator decorators
+  @IsString()
   search?: string
 }
