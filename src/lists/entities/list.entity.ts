bba1747561560d11aea42e6b0ba903bad491a938
¡Duque <atanaelleonardo@gmail.com>
2025-02-04 00:55:06 -0300

    feat: update `list.entity.ts` and `user.entity.ts`

--- src/lists/entities/list.entity.ts
+++ src/lists/entities/list.entity.ts
@@ -1,8 +1,24 @@
-import { ObjectType } from '@nestjs/graphql'
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { User } from 'src/users/entities/user.entity'
+import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
+@Entity({ name: 'lists' })
 @ObjectType()
 export class List {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
   id: string
+
+  @Column()
+  @Field(() => String)
   name: string
-  user
+
+  // Relaciones
+  @ManyToOne(
+    () => User,
+    (user) => user.lists,
+    { nullable: false, lazy: true }
+  ) // * TypeORM decorators
+  @Index('userId-list-index')
+  user: User
 }
