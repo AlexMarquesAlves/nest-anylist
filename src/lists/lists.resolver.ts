f67a720769e1993992873f940c78ed4cae576812
¡Duque <atanaelleonardo@gmail.com>
2025-02-05 20:55:36 -0300

    feat: update lists resolver with authentication and user injection

--- src/lists/lists.resolver.ts
+++ src/lists/lists.resolver.ts
@@ -1,35 +1,43 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { ListsService } from './lists.service';
-import { List } from './entities/list.entity';
-import { CreateListInput } from './dto/create-list.input';
-import { UpdateListInput } from './dto/update-list.input';
+import { UseGuards } from '@nestjs/common'
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { User } from 'src/users/entities/user.entity'
+import { CreateListInput } from './dto/create-list.input'
+import { UpdateListInput } from './dto/update-list.input'
+import { List } from './entities/list.entity'
+import { ListsService } from './lists.service'
 
 @Resolver(() => List)
+@UseGuards(JwtAuthGuard)
 export class ListsResolver {
   constructor(private readonly listsService: ListsService) {}
 
-  @Mutation(() => List)
-  createList(@Args('createListInput') createListInput: CreateListInput) {
-    return this.listsService.create(createListInput);
+  @Mutation(() => List, { name: 'createList' })
+  async createList(
+    @Args('createListInput') createListInput: CreateListInput,
+    @CurrentUser() user: User
+  ): Promise<List> {
+    return this.listsService.create(createListInput, user)
   }
 
   @Query(() => [List], { name: 'lists' })
   findAll() {
-    return this.listsService.findAll();
+    return this.listsService.findAll()
   }
 
   @Query(() => List, { name: 'list' })
   findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.listsService.findOne(id);
+    return this.listsService.findOne(id)
   }
 
   @Mutation(() => List)
   updateList(@Args('updateListInput') updateListInput: UpdateListInput) {
-    return this.listsService.update(updateListInput.id, updateListInput);
+    return this.listsService.update(updateListInput.id, updateListInput)
   }
 
   @Mutation(() => List)
   removeList(@Args('id', { type: () => Int }) id: number) {
-    return this.listsService.remove(id);
+    return this.listsService.remove(id)
   }
 }
