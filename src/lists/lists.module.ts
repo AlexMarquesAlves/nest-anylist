ee3976365596f26f98d6519594ebb9885d3f70f6
¡Duque <atanaelleonardo@gmail.com>
2025-02-05 21:38:21 -0300

    feat: Add TypeORM and list entity to lists module and update schema.gql to include list type and related resolvers and mutations

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { List } from './entities/list.entity'
 import { ListsResolver } from './lists.resolver'
 import { ListsService } from './lists.service'
 
 @Module({
   providers: [ListsResolver, ListsService],
+  imports: [TypeOrmModule.forFeature([List])],
+  exports: [ListsService, TypeOrmModule],
 })
 export class ListsModule {}
