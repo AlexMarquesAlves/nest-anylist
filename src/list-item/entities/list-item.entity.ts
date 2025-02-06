9c382a1beed315a49d6dec41f43637974751c557
¡Duque <atanaelleonardo@gmail.com>
2025-02-05 22:57:15 -0300

    feat: update ListItem entity and module to use TypeORM and update schema.gql

--- src/list-item/entities/list-item.entity.ts
+++ src/list-item/entities/list-item.entity.ts
@@ -1,7 +1,23 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
+@Entity('listItems')
 @ObjectType()
 export class ListItem {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid') //* TypeORM decorators
+  @Field(() => ID) //* GraphQl decorators
+  id: string
+
+  @Column({ type: 'numeric' }) //* TypeORM decorators
+  @Field(() => Number) //* GraphQl decorators
+  quantity: number
+
+  @Column({ type: 'boolean' }) //* TypeORM decorators
+  @Field(() => Boolean) //* GraphQl decorators
+  completed: boolean
+
+  // TODO: Relaciones
+  // list: List
+
+  // item: Item
 }
--- src/list-item/list-item.module.ts
+++ src/list-item/list-item.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { ListItemService } from './list-item.service';
-import { ListItemResolver } from './list-item.resolver';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ListItem } from './entities/list-item.entity'
+import { ListItemResolver } from './list-item.resolver'
+import { ListItemService } from './list-item.service'
 
 @Module({
   providers: [ListItemResolver, ListItemService],
+  imports: [TypeOrmModule.forFeature([ListItem])],
+  exports: [ListItemResolver, TypeOrmModule],
 })
 export class ListItemModule {}
