ef9a61e687a1ea6dd294c8ef0bdfd7b8d76a2177
¡Duque <atanaelleonardo@gmail.com>
2025-02-08 00:13:51 -0300

    refactor: removed unused code and commented out unused functions in ListItemResolver and ListItemService

--- src/list-item/list-item.resolver.ts
+++ src/list-item/list-item.resolver.ts
@@ -1,35 +1,40 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { ListItemService } from './list-item.service';
-import { ListItem } from './entities/list-item.entity';
-import { CreateListItemInput } from './dto/create-list-item.input';
-import { UpdateListItemInput } from './dto/update-list-item.input';
+import { UseGuards } from '@nestjs/common'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { CreateListItemInput } from './dto/create-list-item.input'
+import { ListItem } from './entities/list-item.entity'
+import { ListItemService } from './list-item.service'
 
 @Resolver(() => ListItem)
+@UseGuards(JwtAuthGuard)
 export class ListItemResolver {
   constructor(private readonly listItemService: ListItemService) {}
 
   @Mutation(() => ListItem)
-  createListItem(@Args('createListItemInput') createListItemInput: CreateListItemInput) {
-    return this.listItemService.create(createListItemInput);
+  createListItem(
+    @Args('createListItemInput') createListItemInput: CreateListItemInput
+    //? Se puede pedir el usuario para validarlo
+  ): Promise<ListItem> {
+    return this.listItemService.create(createListItemInput)
   }
 
-  @Query(() => [ListItem], { name: 'listItem' })
-  findAll() {
-    return this.listItemService.findAll();
-  }
+  // @Query(() => [ListItem], { name: 'listItem' })
+  // findAll() {
+  //   return this.listItemService.findAll();
+  // }
 
-  @Query(() => ListItem, { name: 'listItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.findOne(id);
-  }
+  // @Query(() => ListItem, { name: 'listItem' })
+  // findOne(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.findOne(id);
+  // }
 
-  @Mutation(() => ListItem)
-  updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
-    return this.listItemService.update(updateListItemInput.id, updateListItemInput);
-  }
+  // @Mutation(() => ListItem)
+  // updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
+  //   return this.listItemService.update(updateListItemInput.id, updateListItemInput);
+  // }
 
-  @Mutation(() => ListItem)
-  removeListItem(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.remove(id);
-  }
+  // @Mutation(() => ListItem)
+  // removeListItem(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.remove(id);
+  // }
 }
--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateListItemInput } from './dto/create-list-item.input';
-import { UpdateListItemInput } from './dto/update-list-item.input';
+import { Injectable, Logger } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { CreateListItemInput } from './dto/create-list-item.input'
+import { UpdateListItemInput } from './dto/update-list-item.input'
+import { ListItem } from './entities/list-item.entity'
 
 @Injectable()
 export class ListItemService {
-  create(createListItemInput: CreateListItemInput) {
-    return 'This action adds a new listItem';
+  private logger = new Logger('ListItemService')
+
+  constructor(
+    @InjectRepository(ListItem)
+    private readonly listItemRepository: Repository<ListItem>
+  ) {}
+
+  async create(createListItemInput: CreateListItemInput) {
+    const { itemId, listId, ...rest } = createListItemInput
+    const newListItem = this.listItemRepository.create({
+      ...rest,
+      item: { id: itemId },
+      list: { id: listId },
+    })
+
+    this.logger.log(`New list item "${newListItem.id}" was successfully created`)
+    return this.listItemRepository.save(newListItem)
   }
 
   findAll() {
-    return `This action returns all listItem`;
+    return `This action returns all listItem`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} listItem`;
+    return `This action returns a #${id} listItem`
   }
 
   update(id: number, updateListItemInput: UpdateListItemInput) {
-    return `This action updates a #${id} listItem`;
+    return `This action updates a #${id} listItem`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} listItem`;
+    return `This action removes a #${id} listItem`
   }
 }
