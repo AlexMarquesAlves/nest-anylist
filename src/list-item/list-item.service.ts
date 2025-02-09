4521beb8633aeaffa769601cecef1ab1ec272d2c
¡Duque <atanaelleonardo@gmail.com>
2025-02-08 23:50:04 -0300

    feat: update ListItemService to use query builder for update operation

--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { PaginationArgs, SearchArgs } from '../common/dto/args'
@@ -67,17 +67,18 @@ export class ListItemService {
 
   async update(id: string, updateListItemInput: UpdateListItemInput): Promise<ListItem> {
     const { itemId, listId, ...rest } = updateListItemInput
-    const listItem = await this.listItemRepository.preload({
-      ...rest,
-      item: { id: itemId },
-      list: { id: listId },
-    })
-    if (!listItem) {
-      this.logger.error(`ListItem with id ${id} not found`)
-      throw new NotFoundException(`ListItem with id ${id} not found`)
-    }
+    const queryBuilder = this.listItemRepository
+      .createQueryBuilder()
+      .update()
+      .set(rest)
+      .where(`id = :id`, { id })
 
-    return this.listItemRepository.save(listItem)
+    if (listId) queryBuilder.set({ list: { id: listId } })
+    if (itemId) queryBuilder.set({ item: { id: itemId } })
+
+    await queryBuilder.execute()
+
+    return this.findOne(id)
   }
 
   remove(id: number) {
