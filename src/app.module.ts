cbebb50e67f70d6eb9676e26184590de97d22233
¡Duque <atanaelleonardo@gmail.com>
2025-02-14 18:24:35 -0300

    feat: updated project-words.txt and app.module.ts with new imports and configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
-import { join } from 'node:path'
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { JwtService } from '@nestjs/jwt'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'node:path'
 import { AuthModule } from './auth/auth.module'
 import { CommonModule } from './common/common.module'
 import { ItemsModule } from './items/items.module'
+import { ListItemModule } from './list-item/list-item.module'
 import { ListsModule } from './lists/lists.module'
 import { SeedModule } from './seed/seed.module'
 import { UsersModule } from './users/users.module'
-import { ListItemModule } from './list-item/list-item.module';
 
 @Module({
   imports: [
@@ -42,7 +42,11 @@ import { ListItemModule } from './list-item/list-item.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      ssl:
+        process.env.STATE === 'prod'
+          ? { rejectUnauthorized: false, sslmode: 'require' }
+          : (false as any),
+      host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
@@ -61,4 +65,15 @@ import { ListItemModule } from './list-item/list-item.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  private logger = new Logger('AppModule')
+
+  constructor() {
+    this.logger.log('STATE', process.env.STATE)
+    this.logger.log('host', process.env.DB_HOST)
+    this.logger.log('port', +process.env.DB_PORT)
+    this.logger.log('username', process.env.DB_USERNAME)
+    this.logger.log('password', process.env.DB_PASSWORD)
+    this.logger.log('database', process.env.DB_NAME)
+  }
+}
